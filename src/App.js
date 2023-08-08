735e6e8b0ae0997e090bb4ee0dfec5d426574d69
mohit9934059912 <mohitkumarjeh309@gmail.com>
2023-08-09 01:14:02 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+let name = "mohit";
+function App() {
+  return (
+    <>
+   <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Login</li>
+   </nav>
+   <div className="container">
+    <h1>hello {name}</h1>
+    hello everyone this is mohit kumar 
+    from nit rourkela,odisha.
+   </div>
+    </>
+    
+  );
+}
+
+export default App;
